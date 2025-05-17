3d9cf2714b4fa5a2736a47bb492d6f273744739c
ahmed <amhmdslah104@gmail.com>
2025-05-18 01:54:29 +0300

    fix api routes wronge use

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\ApplicationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ApplicationController;
 use App\Http\Controllers\EmployerController;
 use App\Http\Controllers\JobCommentController;
 use App\Http\Controllers\JobController;
