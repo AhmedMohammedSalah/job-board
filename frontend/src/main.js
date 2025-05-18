b16704ef7fe61be9ce223ddd2ace7d91fcb7c78d
ahmed <amhmdslah104@gmail.com>
2025-05-18 21:59:09 +0300

    merge with rehab code <auth>

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,17 @@
-import { createApp } from 'vue'
-<<<<<<< HEAD
-=======
-import './style.css'
+import { createApp } from "vue";
+import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
->>>>>>> 1f4d411 (axios init)
-import App from './App.vue'
-import router from './router'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import App from "./App.vue";
+import router from "./router";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-createApp(App)
-  .use(router)
-  .mount('#app')
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
