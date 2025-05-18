b029943d380ab57a0ada5c4daafc17784385421f
ahmed <amhmdslah104@gmail.com>
2025-05-18 21:44:41 +0300

    axios init

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+<<<<<<< HEAD
+=======
+import './style.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+>>>>>>> 1f4d411 (axios init)
 import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
