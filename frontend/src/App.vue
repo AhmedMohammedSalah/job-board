8669ac159b67e9d9f671b1078e773ee49eb01341
ahmed <amhmdslah104@gmail.com>
2025-05-19 18:01:07 +0300

    edit alias @

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,5 @@
 
-<script setup>
-import AppHeader from "./components/homePage/AppHeader.vue";
-import AppFooter from "./components/homePage/AppFooter.vue";
-import { RouterView } from "vue-router";
-</script>
+
 <template>
   <div>
     <AppHeader />
